2b6c4a55351b6e96a1c8edca9d6fea006aceac98
Kevinsaez <kevinsaez10@gmail.com>
2023-10-25 04:31:28 -0300

    home titulo y parrafo

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,15 @@
 
 export default function Home() {
     return (
-      <div>
-      
-      </div>
+      <header className="container-fluid header-contenedor">
+        <div className=" align-items-center justify-content-center d-flex flex-column p-4 w-4">
+          <h1>Taller el Garage</h1>
+          <p>"¡Bienvenidos a <strong>Taller el Garage!</strong> En nuestro taller, nos enorgullece ofrecer un servicio de mecánica de confianza y de alta calidad. Con años de experiencia en el sector, hemos perfeccionado nuestras habilidades para garantizar que su vehículo reciba el mejor cuidado posible. Nuestro compromiso con la excelencia se refleja en cada proyecto que emprendemos, desde reparaciones de precisión hasta un mantenimiento preventivo que mantiene su automóvil en las mejores condiciones. En <strong>Taller el Garage</strong>, su satisfacción y la seguridad de su vehículo son nuestra principal prioridad. Estamos aquí para atender todas sus necesidades automotrices y brindarle tranquilidad en la carretera."
+          </p>
+          <div>
+
+          </div>
+        </div>
+      </header>
     );
   }
