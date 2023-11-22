d4e3c923dfcba04e1dd2e43c5b83219ca1cd2d9b
Kevinsaez <kevinsaez10@gmail.com>
2023-11-22 05:02:17 -0300

    form-register

--- src/components/formRegister/Register.js
+++ src/components/formRegister/Register.js
@@ -1,29 +1,28 @@
-
+import '../formRegister/registerEstilo.css'
 export default function Register() {
     return (
         
-            <div className="container aling-items-center justify-content-center d-flex m-50 bg-dark">
-               <form>
+            <div className="container  registerContainer aling-items-center justify-content-center d-flex flex-column bg-dark">
+               <form action='' className="m-5 d-flex flex-column aling-items-center justify-content-center">
                 <div className="row mb-3">
-                    <label for="inputEmail3" className="col-sm-2 col-form-label">Email</label>
-                    <div className="col-sm-10">
-                        <input type="email" className="form-control" id="inputEmail3"/>
+                    <div className="col-sm-10 w-100">
+                        <input type="email" className="form-control" id="inputEmail" placeholder='Email' required/>
                     </div>
                 </div>
                 <div className="row mb-3">
-                    <label for="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-                    <div className="col-sm-10">
-                        <input type="password" className="form-control" id="inputPassword3"/>
+                    <div className="col-sm-10 w-100">
+                        <input type="text" className="form-control" id="inputUserName" placeholder='User Name' required/>
+                        
                     </div>
                 </div>
-                <div className="row mb-3">
-                    <label for="inputPassword3" className="col-sm-2 col-form-label">Password</label>
-                    <div className="col-sm-10">
-                        <input type="password" className="form-control" id="inputPassword3"/>
+                <div className="row mb-3 ">
+                    
+                    <div className="col-sm-10 w-100">
+                        <input type="password" className="form-control" id="inputPassword" placeholder='Password' required/>
                     </div>
                 </div>
-                <div className="d-flex gap-5">
-                    <button type="submit" className="btn btn-primary">Sign in</button>
+                <div className="d-flex gap-5 align-items-center justify-content-center">
+                    <button type="submit" className="btn btn-primary">Login</button>
                     <button type="submit" className="btn btn-primary">Sign in</button>
                 </div>
                 </form>
