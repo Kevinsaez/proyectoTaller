897c00b7e2019b66378d3a4029a4e62243d86a4e
Kevinsaez <kevinsaez10@gmail.com>
2023-12-21 21:12:07 -0300

    formCargaCliente

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // import Register from './components/formRegister/Register';
 import Clientes from './pages/clientes/Clientes'
 // import Home from './pages/Home/Home'
+import FormCargaCliente from './components/formCargaCliente/fromCargaCliente'
 export default function App() {
   
 
@@ -12,7 +13,8 @@ export default function App() {
       {/* <Home/> */}
       <Clientes/>
       {/* <Register/> */}
-
+      <FormCargaCliente/>
+     
      
    
 
